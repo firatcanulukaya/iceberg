82ad91649de8c0ce3a78d72aa0cff5b997b2edcc
firatcanulukaya <mua.firat@gmail.com>
2022-07-05 11:14:32 +0300

    css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import "./localization/config"
+import './assets/css/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
