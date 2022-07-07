e756d16bc27b498cc0e851b0ce3770abe2b67bf7
firatcanulukaya <mua.firat@gmail.com>
2022-07-07 16:37:34 +0300

    new color + fixed scroll

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -10,11 +10,6 @@ const Chat = ({user}) => {
     const [messages, setMessages] = useState([]);
     const [loading, setLoading] = useState(true)
     const messagesRef = collection(db, "messages");
-    let bottomListRef = document.getElementById('messagesUl')
-
-    const handleOnChange = e => {
-        setNewMessage(e.target.value);
-    };
 
     const handleOnSubmit = async e => {
         e.preventDefault();
@@ -27,7 +22,8 @@ const Chat = ({user}) => {
             uid: user.uid,
             emailVerified: user.emailVerified
         })
-        bottomListRef.scrollTo(0, bottomListRef.scrollHeight)
+
+        document.getElementById('messagesUl').scrollTo(0, document.getElementById('messagesUl').scrollHeight)
     };
 
     useEffect(() => {
@@ -35,31 +31,36 @@ const Chat = ({user}) => {
             const data = await getDocs(messagesRef);
             await setMessages(data.docs.map(doc => ({...doc.data(), id: doc.id})))
             setLoading(false)
-            bottomListRef = document.getElementById('messagesUl')
-            bottomListRef.scrollTo(0, bottomListRef.scrollHeight)
+            document.getElementById('messagesUl').scrollTo(0, document.getElementById('messagesUl').scrollHeight)
         }
         fetchData()
     }, [])
 
     useEffect(() => {
-        return onSnapshot(collection(db, "messages"), (snapshot) => {
-            setMessages(snapshot.docs.map(doc => ({...doc.data(), id: doc.id})))
-            bottomListRef.scrollTo(0, bottomListRef.scrollHeight)
+        return onSnapshot(collection(db, "messages"), async (snapshot) => {
+            await setMessages(snapshot.docs.map(doc => ({...doc.data(), id: doc.id})))
+            document.getElementById('messagesUl').scrollTo(0, document.getElementById('messagesUl').scrollHeight)
         });
     }, [])
 
+    useEffect(() => {
+        if (messages.length > 0) document.getElementById('messagesUl').scrollTo(0, document.getElementById('messagesUl').scrollHeight)
+    }, [messages])
+
+    if (loading) return <div className="flex justify-center items-center h-[100vh] w-full"><Spinner/></div>
     return (
         <div className="w-full lg:w-1/2 border-l-2 border-r-2 border-solid border-secondaryColor h-[100vh] mx-auto">
             <div className="flex flex-col h-full">
                 <div className="overflow-auto h-full" id="messagesUl">
                     <div className="py-4 max-w-screen-lg mx-auto">
-                        <div className="border-b dark:border-gray-600 border-gray-200 py-8 mb-4 flex flex-col justify-center">
+                        <div
+                            className="border-b dark:border-gray-600 border-gray-200 py-8 mb-4 flex flex-col justify-center">
                             <div className="font-bold text-3xl text-center">
 
-                                <p className="mb-1 text-gray-300">Welcome to</p>
+                                <p className="mb-1 text-gray-300 font-thin">Welcome to</p>
                                 <span className="flex justify-center gap-2">
                                     <img src={Iceberg} alt="Iceberg Logo" className="w-8 h-8"/>
-                                    <p className="mb-3 text-gray-300">Iceberg</p>
+                                    <p className="mb-3 text-gray-300 font-bold">Iceberg</p>
                                 </span>
                             </div>
                             <p className="text-gray-500 text-center">
@@ -83,11 +84,11 @@ const Chat = ({user}) => {
                     </div>
                 </div>
                 <div className="mb-6 mx-4 flex items-center gap-4">
-                    <span className="dropdown group bg-secondaryColor rounded-full px-2 py-1" tabIndex={0}>
+                    <span className="dropdown group bg-secondaryColor rounded-full px-3 py-1" tabIndex={0}>
                                     <button className="dropdown-button">
                                         <img src={user?.photoURL} alt="User Photo"
                                              className="rounded-full w-8 h-8 mr-2"/>
-                                        <i className="dropdown-icon ml-2 h-5 w-5 fa-solid fa-caret-down text-white"/>
+                                        <i className="dropdown-icon ml-2 h-5 w-5 fa-solid fa-caret-down text-gray-400"/>
                                     </button>
                                     <div className="dropdown-content shadow-lg">
                                         <p onClick={logout}
@@ -99,14 +100,14 @@ const Chat = ({user}) => {
                         <input
                             type="text"
                             value={newMessage}
-                            onChange={handleOnChange}
+                            onChange={e => setNewMessage(e.target.value)}
                             placeholder="Type your message here..."
                             className="flex-1 bg-transparent outline-none"/>
                         <button
                             type="submit"
                             disabled={!newMessage}
-                            className="uppercase font-semibold cursor-pointer text-sm tracking-wider text-gray-500 hover:text-white transition-colors disabled:text-gray-500 disabled:cursor-not-allowed">
-                            Send
+                            className="font-semibold cursor-pointer text-lg text-thirdColor transition-colors disabled:text-gray-500 disabled:cursor-not-allowed">
+                            <i className="fa-solid fa-paper-plane"/>
                         </button>
                     </form>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
                 primaryColor: 'rgb(37, 47, 63)',
                 secondaryColor: 'rgb(27, 33, 44)',
                 primaryColorAlt: 'rgb(36, 48, 63)',
-                secondaryColorAlt: 'rgb(72, 105, 147)'
+                secondaryColorAlt: 'rgb(72, 105, 147)',
+                thirdColor: 'rgb(85, 164, 248)'
             }
         },
     },
