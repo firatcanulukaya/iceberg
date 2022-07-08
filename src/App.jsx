df7a8c57ab84d455d957844a6f282528d30d40f0
firatcanulukaya <mua.firat@gmail.com>
2022-07-09 00:14:09 +0300

    fix stay on login page after login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React, {useState} from 'react'
 import {Toaster} from "react-hot-toast";
 import Login from "./components/Login/Login";
 import Chat from "./components/Chat/Chat";
-import Cookies from "js-cookie";
 import {onAuthStateChanged} from "firebase/auth";
 import {auth} from "./firebase";
 
 const App = () => {
     const [user, setUser] = useState(null);
-    const authToken = Cookies.get('_USER_TOKEN_')
 
     onAuthStateChanged(auth, (user) => {
         if (user) {
@@ -21,9 +19,7 @@ const App = () => {
     return (
         <React.Fragment>
             <Toaster position="top-center" reverseOrder={false}/>
-
-            {authToken ? <Chat user={user}/> : <Login/>}
-
+            {user ? <Chat user={user}/> : <Login/>}
         </React.Fragment>
     );
 }
